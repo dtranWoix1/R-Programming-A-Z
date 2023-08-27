1341f438e6c80acaf8df53ebd87d2f0a6f0b3395
dtranWoix1 <134635003+dtranWoix1@users.noreply.github.com>
2023-08-27 06:16:47 -0700

    Add files via upload

---
+++ Module/1.R
@@ -0,0 +1,65 @@
+
+# integer
+x <- 2L
+typeof(x)
+
+#double
+y <- 2.5
+typeof(y)
+
+# complex
+z <- 3 +2i
+
+#character
+
+a <-"h"
+typeof(a)
+
+#logical
+
+q1 <- T
+typeof(q1)
+q2 <- FALSE
+typeof(q2)
+
+A <-10
+B <- 5
+
+C <- A+ B
+C
+
+var1 <- 2.5
+var2 <- 4
+
+result <- var1 /var2
+result
+
+answer <- sqrt(var2)
+answer
+
+greeting <-"Hello"
+name <- "BOB"
+message <- paste(greeting,name)
+message
+
+4 <5
+10>100
+4==5
+
+result <- 4<5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
+
+#while loop
+
+while(FALSE){
+  print("Hello")
+}
---
+++ Module/3.R
@@ -0,0 +1,15 @@
+4 <5
+10>100
+4==5
+
+result <- 4<5
+result
+typeof(result)
+
+result2 <- !(5>1)
+result2
+
+result | result2
+result & result2
+
+isTRUE(result)
